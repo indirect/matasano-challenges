71b868a1a038c4cf76ca3d79196284ac441d82ea
Andre Arko <andre@arko.net>
2014-01-19 15:44:10 -0800

    add test for decrypt::xor_byte

---
+++ src/decrypt/test.rs
@@ -0,0 +1,10 @@
+extern mod decrypt;
+
+#[test]
+fn test_decrypt_xor_byte() {
+    let encrypted = ~"1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+    let answer = decrypt::xor_byte(encrypted).unwrap();
+
+    assert_eq!(answer.text, ~"Cooking MC's like a pound of bacon")
+    assert_eq!(answer.key, ~"X")
+}
