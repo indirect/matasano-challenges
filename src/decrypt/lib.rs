9eaba2696c4f3a6bf955b063f96a62d50fb411d5
Andre Arko <andre@arko.net>
2014-01-19 16:05:30 -0800

    iterators! they are the best

--- src/decrypt/lib.rs
+++ src/decrypt/lib.rs
@@ -13,25 +13,19 @@ pub struct Answer {
 }
 
 pub fn xor_byte(encrypted: &str) -> Option<Answer> {
-    let mut best: Answer = Answer { score: 0, text: ~"", key: ~"" };
+    let bytes = encrypted.from_hex().unwrap();
 
-    for cand in range(0u8, 255u8) {
-        let attempt = encrypted.from_hex().unwrap().xor_byte(cand);
-        let score = english::score(attempt);
-        let atstr = str::from_utf8_owned_opt(attempt);
-
-        if atstr.is_some() {
-            if score > best.score {
-                best.score = score;
-                best.text = atstr.unwrap().clone();
-                best.key = str::from_utf8_owned(~[cand]);
-            }
+    let mut possibles = range(0u8, 255u8).filter_map(|cand| {
+        let xor_bytes = bytes.xor_byte(cand);
+        match str::from_utf8_owned_opt(xor_bytes.clone()) {
+            Some(possible) => Some(Answer {
+                text: possible,
+                score: english::score(xor_bytes),
+                key: str::from_utf8_owned(~[cand])
+            }),
+            None => None
         }
-    }
+    });
 
-    if best.score > 0 {
-        Some(best)
-    } else {
-        None
-    }
+    possibles.max_by(|answer| answer.score )
 }
