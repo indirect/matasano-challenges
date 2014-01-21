310ce1c0e089eb83abbb121a174df564b29706ce
Andre Arko <andre@arko.net>
2014-01-20 19:22:54 -0800

    write program that xors strings with given key

---
+++ src/set1/problem5/main.rs
@@ -0,0 +1,16 @@
+extern mod extra;
+extern mod xor;
+
+use extra::hex::{ToHex};
+use xor::Xor;
+
+fn main() {
+    let args = std::os::args();
+    let plain  = args.get_opt(1).unwrap().as_bytes();
+    let key    = args.get_opt(2).unwrap().as_bytes();
+    let cipher = plain.xor(key);
+
+    println!("plaintext:  {}", plain.to_hex());
+    println!("key:        {}", key.to_hex());
+    println!("ciphertext: {}", cipher.to_hex());
+}
