25264296e3a276cd71d8534d566c1c413dc52f53
Andre Arko <andre@arko.net>
2014-02-07 17:15:11 +0200

    rust returns an option now

--- src/hex/test.rs
+++ src/hex/test.rs
@@ -5,7 +5,7 @@ use hex::{from_hex, to_hex};
 #[test]
 fn test_from_hex() {
     let hex_bytes = from_hex("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
-    let ascii = std::str::from_utf8_owned(hex_bytes);
+    let ascii = std::str::from_utf8_owned(hex_bytes).unwrap();
     assert_eq!(~"I'm killing your brain like a poisonous mushroom", ascii);
 }
 
