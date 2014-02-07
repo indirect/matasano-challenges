412725142637e81a4f7f6498ddf42ba7b086fe22
Andre Arko <andre@arko.net>
2014-02-07 17:14:58 +0200

    switched hamming to strings

--- src/hamming/test.rs
+++ src/hamming/test.rs
@@ -2,6 +2,6 @@ extern mod hamming;
 
 #[test]
 fn test_hamming_distance() {
-    let distance = hamming::distance(bytes!("this is a test"), bytes!("wokka wokka!!!"));
+    let distance = hamming::distance("this is a test", "wokka wokka!!!");
     assert_eq!(37, distance);
 }
