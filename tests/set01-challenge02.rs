bf6e8e121d1f27d4fca574dc037ba51aa27c7e14
Andre Arko <andre@arko.net>
2014-12-26 21:38:16 -0600

    clearer xoring

--- tests/set01-challenge02.rs
+++ tests/set01-challenge02.rs
@@ -8,6 +8,8 @@ use xor::Xor;
 fn set01_challenge02() {
     let source = "1c0111001f010100061a024b53535009181c";
     let other = "686974207468652062756c6c277320657965";
-    let result = source.from_hex().unwrap().xor(&other.from_hex().unwrap());
+    let source_bytes = source.from_hex().unwrap();
+    let other_bytes = other.from_hex().unwrap();
+    let result = source_bytes.xor(&other_bytes);
     assert_eq!(result.to_hex(), "746865206b696420646f6e277420706c6179");
 }
