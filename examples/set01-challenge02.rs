e79dd08562fd7593d4bcb7b22ac0fc0e373eccf8
Andre Arko <andre@arko.net>
2014-12-26 19:51:34 -0600

    challenge 2 passing

---
+++ examples/set01-challenge02.rs
@@ -0,0 +1,15 @@
+extern crate serialize;
+extern crate xor;
+
+use serialize::hex::{FromHex,ToHex};
+use xor::Xor;
+
+fn main() {
+    let source = "1c0111001f010100061a024b53535009181c";
+    println!("Hex:    {}", source);
+    let other = "686974207468652062756c6c277320657965";
+    println!("Xor:    {}", other);
+    let result = source.from_hex().unwrap().xor(&other.from_hex().unwrap());
+    println!("Result: {}", result.to_hex());
+    println!("Text:   {}", String::from_utf8(result).unwrap());
+}
