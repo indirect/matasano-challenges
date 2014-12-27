ea2d243e3308f5778340e3af6705c6bfe65b8338
Andre Arko <andre@arko.net>
2014-12-26 19:51:25 -0600

    remove the unneeded base64 config

--- examples/set01-challenge01.rs
+++ examples/set01-challenge01.rs
@@ -3,24 +3,17 @@ extern crate serialize;
 use serialize::hex::FromHex;
 use serialize::base64::ToBase64;
 
-fn base64_config() -> serialize::base64::Config {
-	serialize::base64::Config {
-		char_set: serialize::base64::CharacterSet::Standard,
-	 	newline: serialize::base64::Newline::LF,
-		pad: false,
-		line_length: None
-	}
-}
-
 fn main() {
-	let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-  println!("{}", hex);
+    let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    println!("Hex:    {}", hex);
 
-	let bytes = match hex.from_hex() {
-		Ok(bytes) => bytes,
-		Err(why) => panic!("String was not hexadecimal: {}", why),
-	};
+    let bytes = match hex.from_hex() {
+        Ok(bytes) => bytes,
+        Err(why) => panic!("String was not hexadecimal: {}", why),
+    };
 
-  let base64 = bytes.to_base64(base64_config());
-	println!("{}", base64);
+    println!("Text:   {}", String::from_utf8(bytes.clone()).unwrap());
+
+    let base64 = bytes.to_base64(serialize::base64::STANDARD);
+    println!("Base64: {}", base64);
 }
