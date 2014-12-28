f4d2221ef561bcef56300a9d2829a80cc91e30e4
Andre Arko <andre@arko.net>
2014-12-28 12:43:48 -0600

    add decrypting repeating xor keys to decrypt_xor

--- src/bin/decrypt_xor.rs
+++ src/bin/decrypt_xor.rs
@@ -8,11 +8,20 @@ use xor::Xor;
 fn main() {
     let args = std::os::args();
 
-    let key = args[1].as_bytes();
-    let cipher = args[2].from_hex().unwrap();
-    let plain = cipher.xor(key);
+    if args.len() < 3 {
+        let cipher = args[1].from_hex().unwrap();
+        let answer = decrypt::xor_repeating(cipher.as_slice());
 
-    println!("ciphertext: {}", cipher.to_hex());
-    println!("key:        {} {}", key.to_hex(), key.to_ascii());
-    println!("plaintext:  {}", String::from_utf8_lossy(plain.as_slice()));
+        println!("ciphertext: {}", cipher.to_hex());
+        println!("key:        {} {}", answer.key.to_hex(), String::from_utf8_lossy(answer.key.as_slice()));
+        println!("plaintext:  {}", String::from_utf8_lossy(answer.bytes.as_slice()));
+    } else {
+        let key = args[1].as_bytes();
+        let cipher = args[2].from_hex().unwrap();
+        let plain = cipher.xor(key);
+
+        println!("ciphertext: {}", cipher.to_hex());
+        println!("key:        {} {}", key.to_hex(), key.to_ascii());
+        println!("plaintext:  {}", String::from_utf8_lossy(plain.as_slice()));
+    }
 }
