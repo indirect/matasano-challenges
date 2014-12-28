cc86f1e67b8af8f6ba57ebf9a487b32a21530b97
Andre Arko <andre@arko.net>
2014-12-28 16:30:19 -0600

    extract count_repeated_blocks into crypto crate

--- examples/set01-challenge08.rs
+++ examples/set01-challenge08.rs
@@ -1,27 +1,10 @@
-extern crate hamming;
 extern crate serialize;
-extern crate decrypt;
+extern crate crypto;
 
 use serialize::hex::FromHex;
 use std::io::BufferedReader;
 use std::io::fs::File;
 use std::path::Path;
-
-fn count_repeated_blocks(bytes: &[u8], block_size: uint) -> uint {
-    let block_count = bytes.len() / block_size as uint;
-
-    let mut repeated_blocks: uint = 0;
-    for block_idx in range(0, block_count) {
-        let block = bytes.slice(block_idx * block_size, (block_idx + 1) * block_size);
-        for other_idx in range(block_idx + 1, block_count) {
-            if block == bytes.slice(other_idx*block_size, (other_idx+1)*block_size) {
-                repeated_blocks += 1;
-            }
-        }
-    }
-
-    repeated_blocks
-}
 
 fn main() {
     let path = Path::new(file!().to_string() + "/../../data/set01-challenge08.txt");
@@ -29,7 +12,7 @@ fn main() {
 
     for (index, hex_line) in reader.lines().enumerate() {
         let bytes = hex_line.unwrap().from_hex().unwrap();
-        let repeated_blocks = count_repeated_blocks(bytes.as_slice(), 16);
+        let repeated_blocks = crypto::count_repeated_blocks(bytes.as_slice(), 16);
 
         if repeated_blocks > 0 {
             println!("line {} repeated {} blocks", index, repeated_blocks);
---
+++ vendor/crypto/src/lib.rs
@@ -0,0 +1,33 @@
+fn count_repeated_blocks(bytes: &[u8], block_size: uint) -> uint {
+    let block_count = bytes.len() / block_size as uint;
+
+    let mut repeated_blocks: uint = 0;
+    for block_idx in range(0, block_count) {
+        let block = bytes.slice(block_idx * block_size, (block_idx + 1) * block_size);
+        for other_idx in range(block_idx + 1, block_count) {
+            if block == bytes.slice(other_idx*block_size, (other_idx+1)*block_size) {
+                repeated_blocks += 1;
+            }
+        }
+    }
+
+    repeated_blocks
+}
+
+#[test]
+fn no_repeated_blocks() {
+    let bytes = vec![1, 2, 3, 4, 5, 6];
+    assert_eq!(0, count_repeated_blocks(bytes.as_slice(), 3));
+}
+
+#[test]
+fn one_repeated_block() {
+    let bytes = vec![1, 1, 2, 3, 4, 5, 1, 1, 6, 7];
+    assert_eq!(1, count_repeated_blocks(bytes.as_slice(), 2));
+}
+
+#[test]
+fn three_repeated_blocks() {
+    let bytes = vec![1, 1, 2, 3, 1, 1, 4, 5, 1, 1, 6, 7];
+    assert_eq!(3, count_repeated_blocks(bytes.as_slice(), 2));
+}
