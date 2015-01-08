c16f3073c065ef3f4d3a3ed305cb8a376fad071d
Andre Arko <andre@arko.net>
2015-01-07 17:47:52 -0800

    fix challenge 9 for namespaced pkcs7

--- examples/set02-challenge09.rs
+++ examples/set02-challenge09.rs
@@ -1,11 +1,13 @@
 extern crate crypto;
 
+use crypto::pkcs7;
+
 fn main() {
     println!("Going to pad string YELLOW SUBMARINE with a block size of 20.");
 
     let submarine = "YELLOW SUBMARINE".as_bytes();
     println!("Bytes  ({}): {}", submarine.len(), submarine);
 
-    let padded_submarine = crypto::pkcs7::pad(submarine.as_slice(), 20);
+    let padded_submarine = pkcs7::pad(submarine.as_slice(), 20);
     println!("Padded ({}): {}", padded_submarine.len(), padded_submarine);
 }
--- tests/set02-challenge09.rs
+++ tests/set02-challenge09.rs
@@ -1,7 +1,8 @@
 extern crate crypto;
+use crypto::pkcs7;
 
 #[test]
 fn set02_challenge09() {
-    let padded_submarine = crypto::pkcs7( "YELLOW SUBMARINE".as_bytes(), 20);
+    let padded_submarine = pkcs7::pad( "YELLOW SUBMARINE".as_bytes(), 20);
     assert_eq!("YELLOW SUBMARINE\x04\x04\x04\x04".as_bytes(), padded_submarine);
 }
